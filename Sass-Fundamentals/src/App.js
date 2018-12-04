18d9b58ca1fb8560d33e223befe6b253d0eed435
anthony62490 <anthonyjmagno@gmail.com>
2018-12-03 21:37:19 -0600

    scripts needed an entry point

---
+++ Sass-Fundamentals/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import T from './components/T'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <T/>
+      </div>
+    );
+  }
+}
+
+export default App;
